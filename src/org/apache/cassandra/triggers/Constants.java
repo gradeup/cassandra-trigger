b257b6fd5383a537570aa5c26a04cd0565231664
sanjeev0291 <sanjeev@gradestack.com>
2017-08-28 10:10:48 +0530

    Update Constants.java

--- src/org/apache/cassandra/triggers/Constants.java
+++ src/org/apache/cassandra/triggers/Constants.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 
 public class Constants {
 
-	public static final String ELASTIC_URL = "10.0.1.55";
-	public static final int ELASTIC_PORT = 9302;
-	public static final String RABBIT_URL = "amqp://localhost";
-	public static final String ELASTIC_CLUSTER_NAME = "elasticsearch";
+	public static final String ELASTIC_URL = "";
+	public static final int ELASTIC_PORT = 9300;
+	public static final String RABBIT_URL = "";
+	public static final String ELASTIC_CLUSTER_NAME = "";
 	public static final HashMap<String, String> ES_INDEX_KEY_MAP;
 	public static final HashMap<String, String> TABLE_INDEX_MAP;
 	public static final HashMap<String, String> INDEX_ROUTING_MAP;
@@ -15,26 +15,14 @@ public class Constants {
 
 	static {
 		ES_INDEX_KEY_MAP = new HashMap<String, String>();
-		ES_INDEX_KEY_MAP.put("user", "email");
-		ES_INDEX_KEY_MAP.put("group_post", "postid");
-		ES_INDEX_KEY_MAP.put("post_counts","postid");
-		ES_INDEX_KEY_MAP.put("group_postcount", "groupid");
-		ES_INDEX_KEY_MAP.put("user_postcount", "email");
-		ES_INDEX_KEY_MAP.put("post_comment","commentid");
-		ES_INDEX_KEY_MAP.put("bookmarks","userid#type#id");
+		ES_INDEX_KEY_MAP.put("index","key1#key2#key3");
+		ES_INDEX_KEY_MAP.put("index2", "key");
 		
 		INDEX_ROUTING_MAP = new HashMap<String, String>();
-		INDEX_ROUTING_MAP.put("group_post", "postid");
-		INDEX_ROUTING_MAP.put("post_counts","postid");
-		INDEX_ROUTING_MAP.put("post_comment","postid");
-		INDEX_ROUTING_MAP.put("bookmarks","userid");
-
+		INDEX_ROUTING_MAP.put("index", "key1");
+		
 		TABLE_INDEX_MAP = new HashMap<String, String>();
 		TABLE_INDEX_MAP.put("table1", "index1");
-		TABLE_INDEX_MAP.put("post_counts","group_post");
-		TABLE_INDEX_MAP.put("group_postcount","group");
-		TABLE_INDEX_MAP.put("user_postcount","user");
-
 	}
 
 }

0fee5fbbd5df0f2f2e68728d11840ffecd9fca01
sanjeev0291 <sanjeev@gradestack.com>
2017-09-13 13:07:56 +0530

    Update Constants.java

--- src/org/apache/cassandra/triggers/Constants.java
+++ src/org/apache/cassandra/triggers/Constants.java
@@ -14,23 +14,38 @@ public class Constants {
 	public static final HashMap<String, String> INDEX_CLUSTER_MAP;
 	public static final String QUEUE_NAME = "CASSANDRA-ES-QUEUE";
 	static {
+		/*Config of all es clusters are stored in ELASTIC_CLUSTER_MAP variable. 
+		Trigger code supports multiple ES clusters if required. 
+		It has a primary cluster and rest can be added which are added as secondary.
+		*/
 		ELASTIC_CLUSTER_MAP= new ArrayList<>();
+		//primary elasticsearch cluster name
 		DEFAULT_ELASTIC_CLUSTER_NAME="elasticsearch";
+		//primary elasticsearch clusterName:ip:port
 		ELASTIC_CLUSTER_MAP.add("elasticsearch:127.0.0.1:9300");
-		//add more es server urls
+		//add secondary mutliple es cluster urls if required
 		ELASTIC_CLUSTER_MAP.add("elasticsearch1:127.0.0.1:9301");
 		ELASTIC_CLUSTER_MAP.add("elasticsearch1:127.0.0.1:9302");
 
-		INDEX_CLUSTER_MAP=new HashMap<String,String>();
+		/*if any index is not avaible on primary elasticsearch and is available on 
+		other elasticsearch cluster, add its index name and es clustername here.
+		*/
+		INDEX_CLUSTER_MAP=new HashMap<String,String>();	
 		INDEX_CLUSTER_MAP.put("index1","elasticsearch1");
 
+		/*For each elasticsearch index, define _id key.
+		*/
 		INDEX_KEY_MAP = new HashMap<String, String>();
 		INDEX_KEY_MAP.put("index","key1#key2#key3");
 		INDEX_KEY_MAP.put("index2", "key");
 
+		/*if there is a routing key associated with es index, define it here.
+		*/
 		INDEX_ROUTING_MAP = new HashMap<String, String>();
 		INDEX_ROUTING_MAP.put("index", "key1");
 
+		/*Define cassandra table to elasticsearch index mapping here.
+		*/
 		TABLE_INDEX_MAP = new HashMap<String, String>();
 		TABLE_INDEX_MAP.put("table1", "index1");
 

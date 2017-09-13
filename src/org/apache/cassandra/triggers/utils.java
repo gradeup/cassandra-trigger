d19703aacee5767fd9bf8adeb0f23196c394ce54
sanjeev0291 <sanjeev@gradestack.com>
2017-09-13 13:11:03 +0530

    Update utils.java

--- src/org/apache/cassandra/triggers/utils.java
+++ src/org/apache/cassandra/triggers/utils.java
@@ -34,7 +34,7 @@ public class utils {
 		Map<String, Object> allKeyValueList = new HashMap<String, Object>();
 		allKeyValueList.putAll(partitionKeyValueList);
 		allKeyValueList.putAll(clusterKeyValueList);
-		String key = Constants.INDEX_KEY_MAP.get(indexColumnFamily);
+		String key = Constants.INDEX_KEY_MAP.get(Constants.TABLE_INDEX_MAP.get(indexColumnFamily));
 		try {
 			if (key != null) {
 				if(!key.contains("#")){

9c87b60378cfb7b086b59fc341699b4cf6caa1c5
sanjeev <sanjeev@gradestack.com>
2018-05-23 06:23:25 +0000

    changed memcached to redis client

--- src/org/apache/cassandra/triggers/Memcached.java
+++ src/org/apache/cassandra/triggers/Memcached.java
@@ -4,23 +4,30 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Future;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.spy.memcached.MemcachedClient;
+import com.lambdaworks.redis.*;
+import com.lambdaworks.redis.api.*;
+import com.lambdaworks.redis.api.sync.*;
+import com.lambdaworks.redis.api.sync.RedisStringCommands;
 
 public class Memcached {
-	private static MemcachedClient mcc;
+	private static RedisClient mcc;
 	private static final Logger logger = LoggerFactory.getLogger(Memcached.class);
 
+	private static RedisStringCommands sync;
+	private static StatefulRedisConnection<String, String> connection;
 	public static Object getValue(String cacheKey) {
 		if(Constants.MEMCACHE_URL==null||Constants.MEMCACHE_URL.length()==0){
 			return null;
 		}
 		try{
 		if(mcc==null){
-			mcc = new MemcachedClient(new InetSocketAddress(Constants.MEMCACHE_URL, Constants.MEMCACHE_PORT));
+			mcc = new RedisClient(RedisURI.create("redis://"+Constants.MEMCACHE_URL+":"+ Constants.MEMCACHE_PORT));
+			connection = mcc.connect();
+			sync = connection.sync();
 		}
-			Object fo = mcc.get(cacheKey);
-			return fo;
+		Object fo = sync.get(cacheKey);
+		logger.info("cache key : "+cacheKey+", value : "+fo);
+		return fo;
 		}catch(Exception e){
 			mcc=null;
 			logger.error("Exception in Memcache : ",e);
